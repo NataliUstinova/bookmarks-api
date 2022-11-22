56ab34130ba340e13c010b0f66805cccafc3d025
Natali Ustinova <nata@purple.studio>
2022-11-22 21:16:36 +0200

    feat: routes users and login #6

---
+++ routes/login.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { createUser, login } = require('../controllers/users');
+
+// роуты, не требующие авторизации, регистрация и логин
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } }),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } }),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.get('/signout', (req, res) => {
+  res.clearCookie('jwt').send({ message: 'Выход' });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,17 @@
-// # возвращает информацию о пользователе (email и имя)
-// GET /users/me
-//
-// # обновляет информацию о пользователе (email и имя)
-// PATCH /users/me
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { auth } = require('../middlewares/auth');
+
+const {
+  getUserInfo, updateUserInfo,
+} = require('../controllers/users');
+
+// роуты с авторизацией
+router.get('/me', auth, getUserInfo);
+
+router.patch('/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } }),
+  }),
+}), updateUserInfo);
