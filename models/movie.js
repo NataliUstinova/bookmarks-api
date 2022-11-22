53d045fbca9447e11472cacf2cf07e22b6d6e654
Natali Ustinova <nata@purple.studio>
2022-11-22 20:42:37 +0200

    feat: movie Schema #5

---
+++ models/movie.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const { urlValidatorPattern } = require('../constants/constants');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlValidatorPattern.test(v);
+      },
+      message: (props) => `${props.value} невалидная ссылка`,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlValidatorPattern.test(v);
+      },
+      message: (props) => `${props.value} невалидная ссылка`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlValidatorPattern.test(v);
+      },
+      message: (props) => `${props.value} невалидная ссылка`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'movie',
+    required: true,
+  },
+  nameRU: {
+  type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
