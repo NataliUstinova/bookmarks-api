b4bfd753fab5b44651c59a9b4455c73fb85380da
Natali Ustinova <nata@purple.studio>
2022-11-22 20:35:10 +0200

    feat: user Schema + errors #4

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+const bcrypt = require('bcryptjs');
+const AuthError = require('../errors/auth-err');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+    minLength: 2,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [isEmail, 'Невалидный email'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Неправильные почта или пароль');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new AuthError('Неправильные почта или пароль');
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
