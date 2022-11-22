997ac1d8e9a92897ba2af83e6a40b34121e457e1
Natali Ustinova <nata@purple.studio>
2022-11-22 19:54:51 +0200

    feat:  create database #3

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+app.use(helmet());
+app.disable('x-powered-by');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+
+app.listen(PORT);
